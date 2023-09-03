a62d9617f0fcb0e2488d9f3e5bd3454b619b5fc8
biekeb <91184065+biekeb@users.noreply.github.com>
2023-09-03 11:47:09 +0200

    Update Home.jsx

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import { useEffect, useRef, useState } from "react";
+import Introduction from "../components/introduction";
+import AboutMe from "../components/aboutMe";
+import MyProjects from "../components/myProjects";
+import MySkills from "../components/mySkills";
+import ContactMe from "../components/contactMe";
 
 export default function Home() {
   return (
     <div>
-      <h1>hello world</h1>
+      {/* <Introduction /> */}
+      <AboutMe />
+      <MyProjects />
+      <MySkills />
+      <ContactMe />
     </div>
   );
 }
