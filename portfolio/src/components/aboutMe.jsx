585d177e98a5c0c6d58e894b30533d2fcaca40da
biekeb <91184065+biekeb@users.noreply.github.com>
2023-09-03 11:46:04 +0200

    Create aboutMe.jsx

---
+++ portfolio/src/components/aboutMe.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div className="about-me">
+      <h1>ABOUT ME</h1>
+      <p>
+        Hi, I'm Bieke, a passionate frontend web developer student. I love
+        turning creative ideas into interactive and user-friendly websites.
+        Currently pursuing my education in Erasmus Hogeschool Brussel, I'm eager
+        to learn, collaborate, and contribute to the world of web development.
+      </p>
+
+      <div className="box-gradient-flex">
+        <div className="box-gradient">
+          <h3>UI UX DESIGN</h3>
+        </div>
+        <div className="box-gradient">
+          <h3>UI UX DESIGN</h3>
+        </div>
+      </div>
+
+      <div>
+        <p>download my resume</p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
