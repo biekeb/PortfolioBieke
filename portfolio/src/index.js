11f0d264a1c01a2de0e23ef29e6385ccdd4b24c5
biekeb <91184065+biekeb@users.noreply.github.com>
2023-09-03 11:46:51 +0200

    creating a route for the page

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
+import "./styles/style.css"
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ErrorPage from './pages/Error';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
   },
 
 ]);
