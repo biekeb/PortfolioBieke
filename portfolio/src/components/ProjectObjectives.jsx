abd71c54dbc5e98a987f8b06ca1daa96bd3de77d
biekeb <91184065+biekeb@users.noreply.github.com>
2023-09-04 19:10:43 +0200

    Create ProjectObjectives.jsx

---
+++ portfolio/src/components/ProjectObjectives.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ProjectObjectives = () => {
+  return (
+    <div className="project-objectives">
+      <h2>Project objectives</h2>
+      <p>
+        To seamlessly integrate a recorded physical award show with an
+        interactive digital platform.
+      </p>
+      <p>
+        To provide attendees and participants with a visually engaging and
+        user-friendly interface to explore event activities.
+      </p>
+    </div>
+  );
+};
+
+export default ProjectObjectives;
