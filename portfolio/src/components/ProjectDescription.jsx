323c61c7859d4f6289a0b7b3d82b5894e7a116cc
biekeb <91184065+biekeb@users.noreply.github.com>
2023-09-04 19:09:52 +0200

    adding a slideshow of pictures to the project page

--- portfolio/src/components/ProjectDescription.jsx
+++ portfolio/src/components/ProjectDescription.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import image from "../images/finalshow.png";
+import image1 from "../images/finalshow1.png"; // Add more images as needed
+import image2 from "../images/finalshow2.png"; // Add more images as needed
+import image3 from "../images/finalshow3.png"; // Add more images as needed
+import { easeLinear } from "d3-ease";
 
 const ProjectDescription = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [image, image1, image2, image3]; // Add more images to the array as needed
+
+  useEffect(() => {
+    const imageInterval = setInterval(() => {
+      // Change the image index every 5 seconds
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000);
+
+    return () => {
+      // Clean up the interval when the component unmounts
+      clearInterval(imageInterval);
+    };
+  }, [images]);
+
   return (
     <div className="project-description">
-      <h1>FINALSHOW</h1>
-      <a href="">
-        <img src={image} alt="" />
-      </a>
-      <p>go to webpage</p>
+      <div className="descr-flex">
+        <div className="descr-item1">
+          <h1 className="rotated-h1">FINALSHOW</h1>
+        </div>
+        <div className="descr-item2">
+          <img
+            src={images[currentImageIndex]}
+            alt=""
+            className="image-transition"
+          />
+          <a href="">
+            <p id="project-link">go to webpage</p>
+          </a>
+        </div>
+      </div>
+
       <p>
         "FinalShow" is a comprehensive project that encompasses a recorded
         physical award show and an innovative interactive application. This
