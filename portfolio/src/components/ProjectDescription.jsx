b031a8142e85a364fdc53bb5e9593b2f1f898f1e
biekeb <91184065+biekeb@users.noreply.github.com>
2023-09-06 20:12:32 +0200

    Adding json data to the description

--- portfolio/src/components/ProjectDescription.jsx
+++ portfolio/src/components/ProjectDescription.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+
 import image from "../images/finalshow.png";
 import image1 from "../images/finalshow1.png"; // Add more images as needed
 import image2 from "../images/finalshow2.png"; // Add more images as needed
 import image3 from "../images/finalshow3.png"; // Add more images as needed
-import { easeLinear } from "d3-ease";
 
 const ProjectDescription = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -23,11 +24,15 @@ const ProjectDescription = () => {
     };
   }, [images]);
 
+  const { topic } = useLoaderData();
+
+  console.log(topic.title);
+
   return (
     <div className="project-description">
       <div className="descr-flex">
         <div className="descr-item1">
-          <h1 className="rotated-h1">FINALSHOW</h1>
+          <h1 className="rotated-h1">{topic.title}</h1>
         </div>
         <div className="descr-item2">
           <img
@@ -41,16 +46,7 @@ const ProjectDescription = () => {
         </div>
       </div>
 
-      <p>
-        "FinalShow" is a comprehensive project that encompasses a recorded
-        physical award show and an innovative interactive application. This
-        initiative was undertaken as part of our college curriculum. The
-        cornerstone of this project was the development of a web application
-        that served as the central hub for the entire award show experience. The
-        website's Landing Page was designed to provide a user-friendly, digital
-        map and world view, showcasing the various activities and events
-        scheduled for our college's Award Show.
-      </p>
+      <p>{topic.description}</p>
     </div>
   );
 };
