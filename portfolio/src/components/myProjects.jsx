909b96e9aa226c2f8b6c5c6f07cfdeea77dde102
biekeb <91184065+biekeb@users.noreply.github.com>
2023-09-06 20:12:08 +0200

    Adding json data to my projects

--- portfolio/src/components/myProjects.jsx
+++ portfolio/src/components/myProjects.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import project1 from "../images/finalshow.png";
+import { getTopics } from "../data/dataservice";
+import { Link, useLoaderData } from "react-router-dom";
+
+export async function loader() {
+  const topics = await getTopics();
+  return { topics };
+}
 
 const MyProjects = () => {
+  const { topics } = useLoaderData();
+  console.log(topics);
+  console.log(topics.image);
+
   return (
     <div className="my-project">
-      <h1>MY PROJECTS</h1>
-      <div className="my-project-img">
-        <img src={project1} alt="" />
+      <div className="descr-item1">
+        <h1>MY PROJECTS</h1>
       </div>
-      <div className="my-project-img">
-        <a href="/project">
-          <img src={project1} alt="" />
-        </a>
-      </div>
-      <h1>MY SKILLS</h1>
+      {topics.map((topic) => (
+        <div className="my-project-flex">
+          <div className="my-project-item">
+            <div className="my-project-img">
+              <Link to={`/project/${topic.id}`}>
+                <img src={`${topic.image}`} alt="" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
